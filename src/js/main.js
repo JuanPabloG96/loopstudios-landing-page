2e9984f3d3ca3bbdaa337c682b329dd7848a6c08
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2024-01-12 18:33:43 -0700

    functions for button behavior

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,21 @@ const openCloseBtn = document.querySelectorAll('.open-close-btn');
 const dropMenu = document.querySelector('.dropdown-menu');
 const menuLinks = document.querySelectorAll('.menu-link');
 
-openCloseBtn.forEach(btn =>{
-  btn.addEventListener('click', ()=>{
-    dropMenu.classList.toggle('show-menu');
-    document.body.classList.toggle('lock-body');
-    window.scrollTo(0,0);
-  })
+function toggleMenu() {
+  dropMenu.classList.toggle('show-menu');
+  document.body.classList.toggle('lock-body');
+  window.scrollTo(0, 0);
+}
+
+function closeMenu() {
+  dropMenu.classList.remove('show-menu');
+  document.body.classList.remove('lock-body');
+}
+
+openCloseBtn.forEach(btn => {
+  btn.addEventListener('click', toggleMenu)
 })
 
-menuLinks.forEach(link =>{
-  link.addEventListener('click', ()=>{
-    dropMenu.classList.remove('show-menu');
-    document.body.classList.toggle('lock-body');
-  })
+menuLinks.forEach(link => {
+  link.addEventListener('click', closeMenu)
 })
